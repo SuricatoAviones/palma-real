25ab7cede8a2f1d39397dbf99d9941a6f662e856
Luis <luis.angel151199@gmail.com>
2023-07-11 18:10:47 -0400

    commit test

---
+++ backend/models/Socio.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const socioSchema = mongoose.Schema(
+  {
+    nombre: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    apellidos: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique:true,
+      trim: true
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    telefono:{
+        type: String,
+        default:null,
+        trim:true
+    },
+    numero_socio: {
+      type: int,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Socio = mongoose.model("Socio", socioSchema);
+export default Socio;
